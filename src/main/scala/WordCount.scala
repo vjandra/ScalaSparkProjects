34e512ada5d1fa231ac812cdeec5ae1c6b5c19f2
Rajiv Jandial <rajiv.jandial@pearson.com>
2016-05-03 01:31:55 +0530

    added code the add the word count from a local file.

--- src/main/scala/WordCount.scala
+++ src/main/scala/WordCount.scala
@@ -2,19 +2,21 @@
   * Created by rajiv jandial on 4/29/16.
   */
 
+import org.apache.spark.SparkContext._
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 
 object WordCount{
   def main(args: Array[String]) {
 
-    val filePath = ""
+    val filePath = "/home/edureka/hellospark_ip"
     val sparkConfig = new SparkConf().setAppName("Word Count").setMaster("local")
     val sparkContext = new SparkContext(sparkConfig)
     val input_file = sparkContext.textFile(filePath)
 
+    val wordCount = input_file.flatMap(line => line.split(" ")).map(word => (word, 1)).reduceByKey((a, b) => a + b)
 
-
+    wordCount.foreach(println)
 
 
 
